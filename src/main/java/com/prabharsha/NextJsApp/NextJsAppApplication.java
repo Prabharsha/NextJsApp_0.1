e0104397e5983c6c3a417c124cf12ca4cf3a0dc0
Prabharsha <pansiluprabharsha1@gamil.com>
2022-11-04 17:38:12 +0530

    project uplaod started

---
+++ src/main/java/com/prabharsha/NextJsApp/NextJsAppApplication.java
@@ -0,0 +1,13 @@
+package com.prabharsha.NextJsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextJsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextJsAppApplication.class, args);
+	}
+
+}
